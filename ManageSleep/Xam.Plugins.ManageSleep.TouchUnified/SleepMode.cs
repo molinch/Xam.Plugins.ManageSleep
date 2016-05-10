a2108c6d676c638e8163b9ce9ae38a7828673fb4
Colin Dabritz <Colin.Dabritz@viewpoint.com>
2016-05-10 16:40:15 -0700

    re-added TouchUnified implementation, slightly tweaked.

---
+++ ManageSleep/Xam.Plugins.ManageSleep.TouchUnified/SleepMode.cs
@@ -0,0 +1,22 @@
+﻿using UIKit;
+
+namespace Xam.Plugins.ManageSleep
+{
+    public class SleepMode : SleepModeBase
+    {
+        /// <summary>
+        /// Activates or desactivates the auto sleep mode. True to activate it (default), False to deactivate it.
+        /// Use with caution: if you deactivated auto sleep you will need to reactivate it.
+        /// DoWithoutSleep and DoWithoutSleepAsync methods are preferred since they automatically resume auto sleep.
+        /// </summary>
+        /// <param name="activateAutoSleepMode">If set to <c>true</c> activate auto sleep mode.</param>
+        public override void ActivateAutoSleepMode(bool activateAutoSleepMode)
+        {
+            UIApplication.SharedApplication.InvokeOnMainThread(() =>
+            {
+                UIApplication.SharedApplication.IdleTimerDisabled = activateAutoSleepMode; // hack to force it see our update...
+                UIApplication.SharedApplication.IdleTimerDisabled = !activateAutoSleepMode;
+            });
+        }
+    }
+}
