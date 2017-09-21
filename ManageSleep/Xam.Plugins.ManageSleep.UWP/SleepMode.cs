be331be4e98b5ac8b2d05892de9c2685a8f8fdde
James Gay <james.gay@viewpoint.com>
2017-09-21 14:20:16 +0100

    Create UWP project.

---
+++ ManageSleep/Xam.Plugins.ManageSleep.UWP/SleepMode.cs
@@ -0,0 +1,32 @@
+﻿using Windows.System.Display;
+
+namespace Xam.Plugins.ManageSleep.UWP
+{
+    public class SleepMode : SleepModeBase
+    {
+        private DisplayRequest _displayRequest;
+
+        /// <summary>
+        /// Activates or deactivates the auto sleep mode. True to activate it (default), False to deactivate it.
+        /// Use with caution: if you deactivated auto sleep you will need to reactivate it.
+        /// DoWithoutSleep and DoWithoutSleepAsync methods are preferred since they automatically resume auto sleep.
+        /// </summary>
+        /// <param name="activateAutoSleepMode">If set to <c>true</c> activates auto sleep mode.</param>
+        public override void ActivateAutoSleepMode(bool activateAutoSleepMode)
+        {
+            if (_displayRequest == null)
+            {
+                _displayRequest = new DisplayRequest();
+            }
+
+            if (activateAutoSleepMode)
+            {
+                _displayRequest.RequestRelease();
+            }
+            else
+            {
+                _displayRequest.RequestActive();
+            }
+        }
+    }
+}
